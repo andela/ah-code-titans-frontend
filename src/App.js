0b1ffb6c4941ff79904c0e390bfdb8680e5d8540
Dennis Macharia <dennismacharia4747@gmail.com>
2019-01-08 11:49:34 +0300

    feat(redux): setup redux
    - add required redux folders
    - add a display message from redux store upon app starting
    
    [Delivers #161967081]

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as WelcomeAction from "./actions/WelcomeAction";
 
-const App = () => (
-  <div>
-    <p>Code Titans Frontend</p>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      welcomeMessage: "Welcome to Authors Haven Frontend"
+    };
+  }
 
-export default App;
+  componentDidMount() {
+    const { actions } = this.props;
+    const { welcomeMessage } = this.state;
+  }
+
+  render() {
+    const { message } = this.props;
+
+    return (
+      <div className="App">
+        <h1>{message}</h1>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  message: PropTypes.string.isRequired,
+  actions: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state, ownState) {
+  return {
+    message: state.welcomeReducer.message
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(WelcomeAction, dispatch)
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
