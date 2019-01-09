1a5a1f9016ab5e3c6dc7e03393a043464cd83ae0
Dennis Macharia <41957873+denomash@users.noreply.github.com>
2019-01-09 12:50:17 +0300

    chore(redux): setup redux (#3)
    - add required redux folders
    - add a display message from redux store upon app starting
    
    [Delivers #161967081]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
+import configureStore from "./store/configureStore";
+import initialState from "./store/initialState";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = configureStore(initialState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import welcomeReducer from "./welcomeReducer";
+
+const rootReducer = combineReducers({
+  welcomeReducer
+});
+
+export default rootReducer;
