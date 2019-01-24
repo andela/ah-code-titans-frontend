25cf9ad85d4dc2517e6468911969198a49c8794b
Jillo Woche <jillo.abdulla@gmail.com>
2019-01-24 19:04:05 +0300

    Fixup rendering

--- src/components/views/articles/SingleArticle.js
+++ src/components/views/articles/SingleArticle.js
@@ -31,7 +31,6 @@ import CreateArticleForm from "./CreateArticleForm";
 import "../../../assets/style/articles/style.scss";
 import "../../../assets/style/articles/bookmark.scss";
 
-const slug = window.location.pathname.slice(9);
 class SingleArticle extends Component {
   constructor(props, context) {
     super(props, context);
