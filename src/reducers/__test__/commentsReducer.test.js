90ed1c569d6a09f38ba1ef98dcf4a5bfb065a19c
daviskimame <hoolykim@gmail.com>
2019-01-25 12:41:10 +0300

    Added test

--- src/reducers/__test__/commentsReducer.test.js
+++ src/reducers/__test__/commentsReducer.test.js
@@ -1,7 +1,44 @@
 import commentReducer from "../commentReducer";
+import * as Types from "../../actions/actionTypes";
 
 describe("test suite for comments reducers", () => {
   it("should handle return default state ", () => {
     expect(commentReducer({}, {})).toEqual({});
+  });
+
+  it("should handle get article comment success ", () => {
+    expect(
+      commentReducer(
+        {
+          comments: []
+        },
+        {
+          type: Types.GET_ARTICLE_COMMENTS_SUCCESS,
+          payload: {
+            comments: ["comment success"],
+            reset: 0
+          }
+        }
+      )
+    ).toEqual({ comments: ["comment success"], hasMore: true, mainOffset: undefined });
+  });
+
+  it("should handle get reply comment success ", () => {
+    expect(
+      commentReducer(
+        {
+          comments: []
+        },
+        {
+          type: Types.GET_REPLY_COMMENT_SUCCESS,
+          payload: {
+            comments: ["comment success"],
+            parentId: 1,
+            offset: 0,
+            reset: false
+          }
+        }
+      )
+    ).toEqual({ comments: ["comment success"] });
   });
 });
