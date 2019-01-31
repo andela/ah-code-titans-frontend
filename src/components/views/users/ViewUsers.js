c1b39ae02310013d88d04e6042bbbc62d9fbe0a5
Darius Ndubi <ndubidarius@gmail.com>
2019-01-31 15:26:21 +0300

    Re-designs list of profiles

--- src/components/views/users/ViewUsers.js
+++ src/components/views/users/ViewUsers.js
@@ -2,25 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import {
-  Card, Image, Icon, Container
+  Card, Image, Grid, Header, Icon
 } from "semantic-ui-react";
 
 const ViewUsers = (props) => {
   const { onUserNameClick, profile } = props;
   return (
-    <Container textAlign="center">
-      <Card>
-        <Image src={profile.image} />
-        <Card.Content>
-          <Card.Header as={Link} to={`/profiles/${profile.username}`} onClick={(event => onUserNameClick(event, profile))}>{profile.username}</Card.Header>
-          <Card.Meta>
-            <Icon className="map marker alternate" />
-            <span>{profile.location}</span>
-          </Card.Meta>
-          <Card.Description>{profile.bio}</Card.Description>
-        </Card.Content>
-      </Card>
-    </Container>
+    <div>
+      <div className="ui text container font">
+        <Grid columns="equal">
+          <Grid.Row className="profile__pa">
+            <Grid.Column>
+              <Header as={Link} to={`/profiles/${profile.username}`} onClick={(event => onUserNameClick(event, profile))}>
+                {profile.username}
+              </Header>
+              <Card.Meta>
+                <Icon className="map marker alternate" />
+                <span>{profile.location}</span>
+              </Card.Meta>
+              <Card.Description>{profile.bio}</Card.Description>
+            </Grid.Column>
+            <Grid.Column>
+              <Image
+                className="profile__image"
+                circular
+                size="tiny"
+                src={profile.image}
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    </div>
   );
 };
 
