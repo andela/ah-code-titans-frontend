391c2d36d04aedaa202fbf22d9662796af21db30
daviskimame <hoolykim@gmail.com>
2019-02-08 11:30:13 +0300

    refactored tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,13 +4,13 @@ import { configure, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router";
-import { configureStoreTest } from "./store/configureStore";
+import configureStore, { configureStoreTest } from "./store/configureStore";
 import initialState from "./store/initialState";
 
 configure({ adapter: new Adapter() });
 
 export function setup(Component, state = initialState) {
-  const { store } = configureStoreTest(state);
+  const { store } = configureStore(state);
   const enzymeWrapper = mount(
     <Provider store={store}>
       <MemoryRouter>{Component}</MemoryRouter>
